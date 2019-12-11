45a9ec71b90dc361328b909c2e9a2f76decfe5cc
MangV2 <58777439+MangV2@users.noreply.github.com>
2019-12-11 22:26:42 +0900

    Add files via upload

---
+++ breed helper.py
@@ -0,0 +1,61 @@
+
+import asyncio
+import discord
+import time 
+
+client = discord.Client()
+#3600초=1시간
+#1800초=30분
+#1500=25분
+
+
+# 토큰입력
+token = "NjU0Mjc5Mjk5MjIxODE1MzE2.XfDPSw.Ye6U0Ix0baEobsMdZkTIwOkf_G0"
+
+
+#봇 준비 완료 메시지
+@client.event 
+async def on_ready():
+    print(client.user.id)
+    print('정상작동! ')
+
+# 봇이 새로운 메시지를 수신했을때
+@client.event
+async def on_message(message):
+    if message.author.bot: #메시지를 보낸 사용자가 봇이라면
+        return None #무시
+#명령어
+@client.event
+async def on_message(message):
+    if message.content.startswith('%파라케 브리딩'):
+        await message.channel.send("```  8시간뒤 알람설정!  ```")
+        time.sleep(27,000)
+        await message.channel.send("30분뒤 파라케 출산!", tts=True)
+        time.sleep(1500)
+        await message.channel.send("5분뒤 파라케 출산!", tts=True)
+
+        if message.content.startswith('%가스백 브리딩'):
+        await message.channel.send("```  8시간뒤 알람설정!  ```")
+        time.sleep(27,000)
+        await message.channel.send("30분뒤 가스백 출산!", tts=True)
+        time.sleep(1500)
+        await message.channel.send("5분뒤 가스백 출산!", tts=True)
+
+        if message.content.startswith('%라이노 브리딩'):
+        await message.channel.send("```  4시간뒤 알람설정!  ```")
+        time.sleep(12,600‬‬)
+        await message.channel.send("30분뒤 라이노 출산!", tts=True)
+        time.sleep(1500)
+        await message.channel.send("5분뒤 라이노 출산!", tts=True)
+        
+        if message.content.startswith('%마나 브리딩'):
+        await message.channel.send("```  4시간뒤 알람설정!  ```")
+        time.sleep(12,600‬‬)
+        await message.channel.send("30분뒤 마나 출산!", tts=True)
+        time.sleep(1500)
+        await message.channel.send("5분뒤 마나 출산!", tts=True)
+
+        
+   
+
+client.run(token)
